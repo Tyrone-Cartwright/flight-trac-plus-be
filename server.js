06a18902c1d1c987a2cc4fab694670d9756f90d0
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2021-10-27 10:09:57 -0400

    wip: server.js setup with dependencies and middleware also connected to mongodb, test route successful

---
+++ server.js
@@ -0,0 +1,40 @@
+//* DEPENDENCIES
+// .env variable
+require('dotenv').config();
+// deconstruct from .env file
+const { PORT, DATABASE_URL } = process.env;
+// import express
+const express = require('express');
+// import mongoose
+const mongoose = require('mongoose');
+const flightController = './controllers/flight.js';
+//* IMPORT MIDDLEWARE
+const cors = require('cors');
+const morgan = require('morgan');
+// create application object
+const app = express();
+// connect to mongoDB
+mongoose.connect(DATABASE_URL);
+// Set up MongoDB listeners
+// Database Connection Error/Success
+const db = mongoose.connection;
+db.on('error', (err) => console.log(err.message + ' is mongodb not running?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
+//* Mount MIDDLEWARE
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+//* Mount controllers
+// app.use('/flight', flightController);
+
+//* Test Route
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+//* Listener
+app.listen(PORT, () =>
+  console.log(`Flights are soaring over Sky-port ${PORT}`)
+);
